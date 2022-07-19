b5817beeb96b6b4e75e018536dbefcfaeeb3c957
ghostly08 <steven.produccionbcn@gmail.com>
2022-07-19 14:40:26 +0200

    Creating components folder and AddCategory component

--- react-gif-expert-app/src/components/AddCategory.jsx
+++ react-gif-expert-app/src/components/AddCategory.jsx
@@ -3,18 +3,25 @@ import { useState } from "react";
 
 export const AddCategory = () => {
 
-  const [inputValue, setInputValue] = useState('One punch');
+  const [inputValue, setInputValue] = useState('One Punch');
 
   const onInputChange = (event) => {
     setInputValue( event.target.value );
+    // console.log(event.target.value);
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault(); // En este caso lo queremos para evitar que se haga un refresh del navegador - esto es default en los forms!!
   }
 
   return (
-    <input
-      type="text"
-      placeholder="gifs searcher"
-      value={ inputValue }
-      onChange={ (event) => onInputChange(event) }
-      />
+    <form onSubmit={ (event) => onSubmit(event) }>
+      <input
+        type="text"
+        placeholder="gifs searcher"
+        value={ inputValue }
+        onChange={ onInputChange } // Whitout this, the value is a read only, two write on the value we need an "onChange"
+        />
+    </form>
   )
 }
